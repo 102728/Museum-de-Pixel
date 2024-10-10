ed71581f65d74a19f42e163193a7501c79c33c7d
aRandomMenno <Menno.van.zoelen@gmail.com>
2024-10-10 14:15:30 +0200

    change index
    * test it rlq on a mac

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Museum-de-Pixel
 Beroeps
+
+## To Do
+
+- [ ] DK main vogel ervoor laten vliegen.
+- [ ] DOOM Gekke demoon laten vliegen.
+- [ ] SONIC gekke ring ervoor laten vliegen.
--- main_index/main.index.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('wheel', function(event) {
-    event.preventDefault();
-}, { passive: false });
-
-document.addEventListener('scroll', function(event) {
-    event.preventDefault();
-}, { passive: false });
